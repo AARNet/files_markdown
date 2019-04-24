22357a631780b94a8582812af1953b52faf95126
Michael Usher <root@usher.is>
2019-04-24 11:36:15 +1000

    Cleanup and refactor

--- js/editor.js
+++ js/editor.js
@@ -1,5 +1,3 @@
-/* global OCA, marked, hljs, MathJax */
-
 OCA.Files_Markdown = {};
 
 OCA.Files_Markdown.mathJaxLoaded = false;
@@ -7,117 +5,129 @@ OCA.Files_Markdown.markedLoadPromise = null;
 OCA.Files_Markdown.highlightLoaded = null;
 
 OCA.Files_Markdown.Preview = function () {
-	this.renderer = null;
-	this.head = document.head;
-	this.preview = _.throttle(this.previewText, 500);
+        this.renderer = null;
+        this.head = document.head;
+        this.preview = _.throttle(this.previewText, 500);
 };
 
-OCA.Files_Markdown.Preview.prototype.init = function () {
-	var getUrl = this.getUrl.bind(this);
+OCA.Files_Markdown.Preview.prototype = {
+        init: function () {
+                var getUrl = this.getUrl.bind(this);
 
-	$.when(
-		this.loadMarked(),
-		this.loadHighlight()
-	).then(function () {
-		this.renderer = new marked.Renderer();
-		this.renderer.image = function (href, title, text) {
-			var out = '<img src="' + getUrl(href) + '" alt="' + text + '"';
-			if (title) {
-				out += ' title="' + title + '"';
-			}
-			out += this.options.xhtml ? '/>' : '>';
-			return out;
-		};
+                $.when(
+                        this.loadMarked(),
+                        this.loadHighlight()
+                ).then(function () {
+                        this.renderer = new marked.Renderer();
+                        this.renderer.image = function (href, title, text) {
+                                var out = '<img src="' + getUrl(href) + '" alt="' + text + '"';
+                                if (title) {
+                                        out += ' title="' + title + '"';
+                                }
+                                out += this.options.xhtml ? '/>' : '>';
+                                return out;
+                        };
 
-		marked.setOptions({
-			highlight: function (code) {
-				return hljs.highlightAuto(code).value;
-			},
-			renderer: this.renderer,
-			headerPrefix: 'md-',
-			sanitize: true
-		});
-	}.bind(this));
-	this.loadMathJax();
+                        marked.setOptions({
+                                highlight: function (code) {
+                                        return hljs.highlightAuto(code).value;
+                                },
+                                renderer: this.renderer,
+                                headerPrefix: 'md-',
+                                sanitize: true
+                        });
+                }.bind(this));
+                this.loadMathJax();
+        },
+
+        getUrl:  function (path) {
+                if (!path) {
+                        return path;
+                }
+                if (path.substr(0, 7) === 'http://' || path.substr(0, 8) === 'https://' || path.substr(0, 3) === '://') {
+                        return path;
+                } else {
+                        if (path.substr(0, 1) !== '/') {
+                                path = OCA.Files_Texteditor.file.dir + '/' + path;
+                        }
+                        return OC.generateUrl('apps/files/ajax/download.php?dir={dir}&files={file}', {
+                                dir: OC.dirname(path),
+                                file: OC.basename(path)
+                        });
+                }
+        },
+
+        previewText: function (text, element) {
+                OCA.Files_Markdown.Preview.addActions();
+                var html = marked(OCA.Files_Markdown.Preview.prepareText(text));
+                element.html(html);
+                if (window.MathJax) {
+                        MathJax.Hub.Queue(["Typeset", MathJax.Hub, element[0]]);
+
+                }
+        },
+
+        loadMarked: function () {
+                if (!OCA.Files_Markdown.markedLoadPromise) {
+                        OCA.Files_Markdown.markedLoadPromise = OC.addScript('files_markdown', 'marked');
+                }
+                return OCA.Files_Markdown.markedLoadPromise;
+        },
+
+        loadHighlight: function () {
+                if (!OCA.Files_Markdown.highlightLoadPromise) {
+                        OCA.Files_Markdown.highlightLoadPromise = OC.addScript('files_markdown', 'highlight.pack');
+                }
+                return OCA.Files_Markdown.highlightLoadPromise;
+        },
+
+        loadMathJax: function () {
+                if (OCA.Files_Markdown.mathJaxLoaded) {
+                        return;
+                }
+                OCA.Files_Markdown.mathJaxLoaded = true;
+                var script = document.createElement("script");
+                script.type = "text/x-mathjax-config";
+                script[(window.opera ? "innerHTML" : "text")] =
+                        "MathJax.Hub.Config({\n" +
+                        "  tex2jax: { inlineMath: [['$','$'], ['\\\\(','\\\\)']] }\n" +
+                        "});";
+                this.head.appendChild(script);
+
+                var path = OC.filePath('files_markdown', 'js', 'mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML');
+
+                //insert using native dom to prevent jquery from removing the script tag
+                script = document.createElement("script");
+                script.src = path;
+                this.head.appendChild(script);
+        }
 };
 
-OCA.Files_Markdown.Preview.prototype.getUrl = function (path) {
-	if (!path) {
-		return path;
-	}
-	if (path.substr(0, 7) === 'http://' || path.substr(0, 8) === 'https://' || path.substr(0, 3) === '://') {
-		return path;
-	} else {
-		if (path.substr(0, 1) !== '/') {
-			path = OCA.Files_Texteditor.file.dir + '/' + path;
-		}
-		return OC.generateUrl('apps/files/ajax/download.php?dir={dir}&files={file}', {
-			dir: OC.dirname(path),
-			file: OC.basename(path)
-		});
-	}
+OCA.Files_Markdown.Preview.prepareText = function (text) {
+        text = text.trim();
+        if (text.substr(0, 3) === '+++') {
+                text = text.substr(3);
+                text = text.substr(text.indexOf('+++') + 3);
+        }
+
+        return text;
 };
 
-OCA.Files_Markdown.Preview.prototype.previewText = function (text, element) {
-	$('<button id="editor_toggle">').text('Toggle Editor').appendTo('#editor_controls');
-	$("#editor_toggle" ).bind( "click", function() {
-  		$('#preview_wrap').toggleClass('editor_hidden');
-  		$('#editor').toggleClass('editor_hidden');
-	});
-	var html = marked(prepareText(text));
-	element.html(html);
-	if (window.MathJax) {
-		MathJax.Hub.Queue(["Typeset", MathJax.Hub, element[0]]);
-	}
+OCA.Files_Markdown.Preview.addActions = function () {
+        editor_controls = $('#editor_controls');
+        if (editor_controls.data('md_toggles') !== 'true') {
+                $('<button id="editor_toggle">').text('Toggle Editor').appendTo('#editor_controls');
+                $("#editor_toggle" ).bind( "click", function() {
+                        $('#preview_wrap').toggleClass('editor_hidden');
+                        $('#editor').toggleClass('editor_hidden');
+                });
+                editor_controls.data('md_toggles', 'true');
+        }
 };
 
-var prepareText = function (text) {
-	text = text.trim();
-	if (text.substr(0, 3) === '+++') {
-		text = text.substr(3);
-		text = text.substr(text.indexOf('+++') + 3);
-	}
-
-	return text;
-};
-
-OCA.Files_Markdown.Preview.prototype.loadMarked = function () {
-	if (!OCA.Files_Markdown.markedLoadPromise) {
-		OCA.Files_Markdown.markedLoadPromise = OC.addScript('files_markdown', 'marked');
-	}
-	return OCA.Files_Markdown.markedLoadPromise;
-};
-
-OCA.Files_Markdown.Preview.prototype.loadHighlight = function () {
-	if (!OCA.Files_Markdown.highlightLoadPromise) {
-		OCA.Files_Markdown.highlightLoadPromise = OC.addScript('files_markdown', 'highlight.pack');
-	}
-	return OCA.Files_Markdown.highlightLoadPromise;
-};
-
-OCA.Files_Markdown.Preview.prototype.loadMathJax = function () {
-	if (OCA.Files_Markdown.mathJaxLoaded) {
-		return;
-	}
-	OCA.Files_Markdown.mathJaxLoaded = true;
-	var script = document.createElement("script");
-	script.type = "text/x-mathjax-config";
-	script[(window.opera ? "innerHTML" : "text")] =
-		"MathJax.Hub.Config({\n" +
-		"  tex2jax: { inlineMath: [['$','$'], ['\\\\(','\\\\)']] }\n" +
-		"});";
-	this.head.appendChild(script);
-
-	var path = OC.filePath('files_markdown', 'js', 'mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML');
-
-	//insert using native dom to prevent jquery from removing the script tag
-	script = document.createElement("script");
-	script.src = path;
-	this.head.appendChild(script);
-};
 
 $(document).ready(function () {
-	if (OCA.Files_Texteditor && OCA.Files_Texteditor.registerPreviewPlugin) {
-		OCA.Files_Texteditor.registerPreviewPlugin('text/markdown', new OCA.Files_Markdown.Preview());
-	}
+        if (OCA.Files_Texteditor && OCA.Files_Texteditor.registerPreviewPlugin) {
+                OCA.Files_Texteditor.registerPreviewPlugin('text/markdown', new OCA.Files_Markdown.Preview());
+        }
 });
