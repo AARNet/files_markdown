4a8afff65f29a16bd8d8e5be6a2c175b00c6edf2
Michael Usher <michael.usher@aarnet.edu.au>
2019-04-24 06:22:57 +0000

    Only load scripts if on Files app

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,8 +1,14 @@
 <?php
-//load the required files
-OCP\Util::addscript( 'files_markdown', 'editor');
-OCP\Util::addStyle( 'files_markdown', 'preview' );
-OCP\Util::addStyle( 'files_markdown', 'highlight-default' );
-OCP\Util::addStyle( 'files_markdown', 'katex' );
-OCP\Util::addStyle( 'files_markdown', 'texmath' );
+
+$eventDispatcher = \OC::$server->getEventDispatcher();
+$eventDispatcher->addListener(
+	'OCA\Files::loadAdditionalScripts',
+	function () {
+		\OCP\Util::addscript( 'files_markdown', 'editor');
+		\OCP\Util::addStyle( 'files_markdown', 'preview' );
+		\OCP\Util::addStyle( 'files_markdown', 'highlight-default' );
+		\OCP\Util::addStyle( 'files_markdown', 'katex' );
+		\OCP\Util::addStyle( 'files_markdown', 'texmath' );
+	}
+);
 
