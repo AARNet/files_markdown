a15c26c0a5271cc1d0e1fec00cd0057566423c08
Michael Usher <michael.usher@aarnet.edu.au>
2019-04-26 01:28:28 +0000

    Update markdown css to be more Github-ish

--- appinfo/app.php
+++ appinfo/app.php
@@ -6,6 +6,7 @@ $eventDispatcher->addListener(
 	function () {
 		\OCP\Util::addscript( 'files_markdown', 'editor');
 		\OCP\Util::addStyle( 'files_markdown', 'preview' );
+		\OCP\Util::addStyle( 'files_markdown', 'markdown' );
 		\OCP\Util::addStyle( 'files_markdown', 'highlight-default' );
 		\OCP\Util::addStyle( 'files_markdown', 'katex' );
 		\OCP\Util::addStyle( 'files_markdown', 'texmath' );
